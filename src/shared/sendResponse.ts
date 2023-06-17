1db1f728154aca44e381513de95a8a468211b2f5
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-06-17 12:57:30 +0600

    [DONE] send response

--- src/shared/sendResponse.ts
+++ src/shared/sendResponse.ts
@@ -1,26 +1,35 @@
 import { Response } from 'express';
 
 type IApiResponse<T> = {
-  statusCode: number;
+  statusCode: number | undefined;
   success: boolean;
-  message?: string | null;
+  message: string | null | undefined;
   meta?: {
     page: number;
     limit: number;
     total: number;
-  };
+  } | undefined;
   data?: T | null;
 };
+
+// data: T | null;
+// success: boolean;
+// meta: { page: number; limit: number; total: number } | undefined;
+// message: string | null | undefined;
+// statusCode: number | undefined
+
 const sendResponse = <T>(res: Response, data: IApiResponse<T>): void => {
   const responseData: IApiResponse<T> = {
-    statusCode: data?.statusCode,
-    success: data?.success,
-    message: data?.message || null,
-    meta: data?.meta || null || undefined,
-    data: data?.data || null,
+    statusCode: data.statusCode,
+    success: data.success,
+    message: data.message,
+    meta: data?.meta ,
+    data: data.data,
   };
 
-  res.status(data?.statusCode).json(responseData);
+  if (data.statusCode != null) {
+    res.status(data.statusCode).json(responseData);
+  }
 };
 
 export default sendResponse;
