6e78a9e70be13bec4498b8568600713111837071
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-06-17 12:34:12 +0600

    [DONE] cow model

--- src/app/modules/cow/cow.model.ts
+++ src/app/modules/cow/cow.model.ts
@@ -7,24 +7,25 @@ import {
   cowLocationConstant,
 } from './cow.constant';
 
-const cowSchema = new Schema<ICow>({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
-  price: { type: Number, required: true },
-  location: { type: String, enum: cowLocationConstant, required: true },
-  breed: { type: String, enum: cowBreadConstant, required: true },
-  weight: { type: Number, required: true },
-  label: { type: String, enum: cowLabelConstant, default: 'for sale' },
-  category: { type: String, enum: cowCategoryConstant, required: true },
-  seller: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-}, {
-  timestamps: true,
-  toJSON: {
-    virtuals: true,
+const cowSchema = new Schema<ICow>(
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    price: { type: Number, required: true },
+    location: { type: String, enum: cowLocationConstant, required: true },
+    breed: { type: String, enum: cowBreadConstant, required: true },
+    weight: { type: Number, required: true },
+    label: { type: String, enum: cowLabelConstant, default: 'for sale' },
+    category: { type: String, enum: cowCategoryConstant, required: true },
+    seller: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   },
-
-
-});
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 // Create and export the Mongoose model
 export const Cow = model<ICow, ICowModel>('Cow', cowSchema);
