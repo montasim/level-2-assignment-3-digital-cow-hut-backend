4f86be7d5f62501837b342b14a8645e54ca0ed56
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-06-17 12:47:36 +0600

    [DONE] user validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import {userTypes} from "./user.constant";
+import { userTypes } from './user.constant';
 
 const createUserZodSchema = z.object({
   body: z.object({
@@ -14,46 +14,60 @@ const createUserZodSchema = z.object({
     }),
     name: z.object({
       firstName: z.string({
-        required_error: 'First name is required'
+        required_error: 'First name is required',
       }),
       lastName: z.string({
-        required_error: 'Last name is required'
+        required_error: 'Last name is required',
       }),
     }),
     address: z.string({
-      required_error: 'Address is required'
+      required_error: 'Address is required',
     }),
     budget: z.number().min(0).optional(),
-    income: z.number().min(0).optional()
-  })
+    income: z.number().min(0).optional(),
+  }),
 });
 
 const updateUserZodSchema = z.object({
   body: z.object({
-    phoneNumber: z.string({
-      required_error: 'Phone number is required',
-    }).optional(),
-    role: z.enum([...userTypes] as [string, ...string[]], {
-      required_error: 'Role is required',
-    }).optional(),
-    password: z.string({
-      required_error: 'Password is required',
-    }).optional(),
-    name: z.object({
-      firstName: z.string({
-        required_error: 'First name is required'
-      }).optional(),
-      lastName: z.string({
-        required_error: 'Last name is required'
-      }).optional(),
-    }).optional(),
-    address: z.string({
-      required_error: 'Address is required'
-    }).optional(),
+    phoneNumber: z
+      .string({
+        required_error: 'Phone number is required',
+      })
+      .optional(),
+    role: z
+      .enum([...userTypes] as [string, ...string[]], {
+        required_error: 'Role is required',
+      })
+      .optional(),
+    password: z
+      .string({
+        required_error: 'Password is required',
+      })
+      .optional(),
+    name: z
+      .object({
+        firstName: z
+          .string({
+            required_error: 'First name is required',
+          })
+          .optional(),
+        lastName: z
+          .string({
+            required_error: 'Last name is required',
+          })
+          .optional(),
+      })
+      .optional(),
+    address: z
+      .string({
+        required_error: 'Address is required',
+      })
+      .optional(),
     budget: z.number().min(0).optional(),
-    income: z.number().min(0).optional()
-  })
-})
+    income: z.number().min(0).optional(),
+  }),
+});
 //   .refine(data=>
 //     (data.body.budget && data.body.income), {
 //       message: "A user can not have both budget and income"
@@ -63,4 +77,4 @@ const updateUserZodSchema = z.object({
 export const UserValidation = {
   createUserZodSchema,
   updateUserZodSchema,
-}
+};
