a634317de8323f80c36980dede10a91621f2e9c7
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-06-17 12:47:10 +0600

    [DONE] user service

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,7 +1,7 @@
 import { User } from './user.model';
 import { IUser } from './user.interface';
 import config from '../../../config';
-import ApiError from "../../../erros/ApiError";
+import ApiError from '../../../erros/ApiError';
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   const createdUser = await User.create(user);
@@ -14,15 +14,18 @@ const createUser = async (user: IUser): Promise<IUser | null> => {
 const getAllUsers = async (): Promise<IUser[]> => {
   const allUsers = await User.find({});
   return allUsers;
-}
+};
 
 const getUser = async (id: string | undefined): Promise<IUser | null> => {
   const user = await User.findById(id);
   return user;
-}
+};
 
-const updateUserData = async (id: string, userData: IUser): Promise<IUser | null> => {
-  if(userData?.budget && userData?.income) {
+const updateUserData = async (
+  id: string,
+  userData: IUser
+): Promise<IUser | null> => {
+  if (userData?.budget && userData?.income) {
     throw new ApiError(400, 'A user can have both budget and income');
   }
   const updatedUserData = await User.findOneAndUpdate({ _id: id }, userData, {
@@ -30,18 +33,18 @@ const updateUserData = async (id: string, userData: IUser): Promise<IUser | null
   });
   ////TODO: user now found
   return updatedUserData;
-}
+};
 
 const deleteUser = async (id: string) => {
   const result = await User.findByIdAndDelete(id);
   ////TODO: user now found
   return result;
-}
+};
 
 export const UserService = {
   createUser,
   getAllUsers,
   getUser,
   updateUserData,
-  deleteUser
+  deleteUser,
 };
