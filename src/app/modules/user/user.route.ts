0dc7f66291e33218459b0a4dff7d29fcb2b0f6d1
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-06-17 12:46:50 +0600

    [DONE] user route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,13 +1,14 @@
-import express from "express";
-import {UserController} from "./user.controller";
-import validateRequest from "../../middlewares/validateRequest";
-import {UserValidation} from "./user.validation";
+import express from 'express';
+import { UserController } from './user.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
 router.get('/', UserController.getUsers);
 router.get('/:id', UserController.getUser);
-router.patch('/:id',
+router.patch(
+  '/:id',
   validateRequest(UserValidation.updateUserZodSchema),
   UserController.updateUser
 );
