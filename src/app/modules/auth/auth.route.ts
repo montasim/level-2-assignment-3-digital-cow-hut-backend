7178b186a8f4c14117b9a549e70b0b9f5ae1a499
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-06-17 12:32:44 +0600

    [DONE] auth route

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,11 +1,12 @@
-import express from "express";
-import validateRequest from "../../middlewares/validateRequest";
-import {UserValidation} from "../user/user.validation";
-import {AuthController} from "./auth.controller";
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { UserValidation } from '../user/user.validation';
+import { AuthController } from './auth.controller';
 
 const router = express.Router();
 
-router.post('/',
+router.post(
+  '/',
   validateRequest(UserValidation.createUserZodSchema),
   AuthController.createUser
 );
