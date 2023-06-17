4450feb052d87928ac58834983317ec408d149d2
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-06-17 12:56:59 +0600

    [DONE] shared pick

--- src/shared/pick.ts
+++ src/shared/pick.ts
@@ -1,12 +1,28 @@
-const pick = <T extends Record<string, unknown>, k extends keyof T>(
+const pick = <T extends Record<string, unknown>, K extends keyof T>(
   obj: T,
-  keys: k[]
+  keys: K[]
 ): Partial<T> => {
   const finalObject: Partial<T> = {};
 
   for (const key of keys) {
     if (obj && Object.hasOwnProperty.call(obj, key)) {
-      finalObject[key] = obj[key];
+      let prevPriceObj = {};
+      if(key === 'minPrice' || key === 'maxPrice') {
+        prevPriceObj = { ...finalObject?.price || {} };
+      }
+      if (key === 'minPrice') {
+        (finalObject as Record<string, unknown>)['price'] = {
+          ...prevPriceObj,
+          $gte: obj[key],
+        };
+      } else if (key === 'maxPrice') {
+        (finalObject as Record<string, unknown>)['price'] = {
+          ...prevPriceObj,
+          $lte: obj[key],
+        };
+      } else {
+        finalObject[key] = obj[key];
+      }
     }
   }
 
